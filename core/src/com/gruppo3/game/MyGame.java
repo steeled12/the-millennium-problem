1a42012e41c3cc8e4af3fa692dcb2614e7c149bb
steeled12 <andrea.fazio02@outlook.it>
2023-11-27 10:04:21 +0100

    Co-authored-by: peppe.rosa02@gmail.com <peppe.rosa02@gmail.com> Co-authored-by: crimeo02@hotmail.com <crimeo02@hotmail.com>

--- core/src/com/gruppo3/game/MyGame.java
+++ core/src/com/gruppo3/game/MyGame.java
@@ -1,22 +1,27 @@
 package com.gruppo3.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.gruppo3.game.screens.TestScreen;
 
-public class MyGame extends ApplicationAdapter {
+public class MyGame extends Game {
 	SpriteBatch batch;
 	Texture img;
-	
+	private MyGame game;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		game = this;
+		setScreen(new TestScreen(game));
 	}
 
 	@Override
-	public void render () {
+	public void render () 
+	{
+		super.render();
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
--- desktop/src/com/gruppo3/game/DesktopLauncher.java
+++ desktop/src/com/gruppo3/game/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("GIocoIss");
+		config.setTitle("GiocoIss");
 		new Lwjgl3Application(new MyGame(), config);
 	}
 }
