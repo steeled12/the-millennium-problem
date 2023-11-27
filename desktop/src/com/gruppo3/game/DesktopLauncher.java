685e6f8882adb9fb391882ace9328892199f3ab9
steeled12 <andrea.fazio02@outlook.it>
2023-11-27 09:32:59 +0100

    Initial commit

---
+++ desktop/src/com/gruppo3/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.gruppo3.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.gruppo3.game.MyGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("GIocoIss");
+		new Lwjgl3Application(new MyGame(), config);
+	}
+}
