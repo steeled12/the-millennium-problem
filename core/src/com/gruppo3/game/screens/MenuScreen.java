fe17d713a1b0b2e5aa2d7f7d6f5dd923d44db3b8
peppe <peppe.rosa02@gmail.com>
2023-12-19 17:17:48 +0100

    added version

--- core/src/com/gruppo3/game/screens/MenuScreen.java
+++ core/src/com/gruppo3/game/screens/MenuScreen.java
@@ -4,12 +4,17 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.gruppo3.game.MyGame;
 import com.gruppo3.game.controller.MenuController;
 
 public class MenuScreen implements Screen {
 
     MenuController menuController;
     private OrthographicCamera camera;
+    MyGame game = (MyGame) Gdx.app.getApplicationListener();
+    Viewport textViewport;
 
     public MenuScreen() {
         this.menuController = new MenuController();
@@ -17,11 +22,12 @@ public class MenuScreen implements Screen {
         camera = new OrthographicCamera();
         camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
         camera.update();
+
+        textViewport = new ScreenViewport();
     }
 
     @Override
     public void show() {
-
     }
 
     @Override
@@ -32,6 +38,11 @@ public class MenuScreen implements Screen {
         menuController.getStage().act();
         menuController.getStage().draw();
         Gdx.input.setInputProcessor(menuController.getStage());
+        game.batch.begin();
+        game.debugFont.draw(game.batch, "VERSION: DEVELOPER-BUILD_0.4",
+                10,
+                20);
+        game.batch.end();
     }
 
     @Override
