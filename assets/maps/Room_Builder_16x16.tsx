ff611213d5e0d946e51ed9f120aa2e49f5d851e9
peppe <peppe.rosa02@gmail.com>
2024-01-09 23:11:26 +0100

    atto1

--- assets/maps/Room_Builder_16x16.tsx
+++ assets/maps/Room_Builder_16x16.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Room_Builder_16x16" tilewidth="16" tileheight="16" tilecount="8284" columns="76">
- <image source="../1_Interiors/16x16/Room_Builder_16x16.png" width="1216" height="1744"/>
+ <image source="1_Interiors/16x16/Room_Builder_16x16.png" width="1216" height="1744"/>
 </tileset>
